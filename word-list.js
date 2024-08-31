c05d29bf685c5cef530aba84b911b599c5bea4dc
brindha-cloud <brindhababy2001@gmail.com>
2024-08-31 12:07:26 +0530

    Add files via upload

---
+++ word-list.js
@@ -0,0 +1,74 @@
+const wordList=[
+    {
+        word: "guitar",
+        hint: "A musical instrument with Strings."
+    },
+    {
+        word: "oxygen",
+        hint: "A colorless,odorless gas essential for Life."
+    },
+    {
+        word: "mountain",
+        hint: "A large natural elevation of the Earth's surface."
+    },
+    {
+        word: "painting",
+        hint: "An art form using colors on a surface to craete images or expression."
+    },
+    {
+        word: "astronomy",
+        hint: "The scientific study of celestical objects and phenomena."
+    },
+    {
+        word: "diamond",
+        hint: "A precious gemstone known for its brilliance and hardness."
+    },
+    {
+        word: "camera",
+        hint: "A device used to capture and record images or videos."
+    },
+    {
+        word: "science",
+        hint: "The systematic study of the structure and behavior of the physical."
+    },
+    {
+        word:"bicycle",
+        hint: "A human-powered vehicle with two wheels."
+    },
+    {
+        word: "adventure",
+        hint: "An exciting or daring experience."
+    },
+    {
+        word: "sunset",
+        hint: "The daily disappearance of the sun below the horizon."
+    },
+    {
+        word: "dance",
+        hint: "A Rhythmic movement of the body often performed to music."
+    },
+    {
+        word: "coffee",
+        hint: "A popular caffeinated beverage made from roasted coffe beans."
+    },
+    {
+        word: "history",
+        hint: "The study of past events and human civilization."
+    },
+    {
+        word: "Pizza",
+        hint: "A savory dish consisiting of a round, flattened base with toppings."
+    },
+    {
+        word: "butterfly",
+        hint: "An insect with colorful wings and a slender body."
+    },
+    {
+        word: "football",
+        hint: "A popular sport played with a spherical ball."
+    },
+    {
+        word: "chocolate",
+        hint: "A sweet treat made from cocoa beans."
+    }
+]
